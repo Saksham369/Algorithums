3220e95a0d39dd0b9b110f56f76a3a10a7d718a7
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-16 17:12:17 +0530

    Add files via upload

---
+++ Seive.java
@@ -0,0 +1,25 @@
+package questions;
+
+public class Seive {
+    public static void main(String[] args){
+     int n = 1000;
+        boolean[] primes = new boolean[n+1];
+        primes(n,primes);
+    }
+
+    static void primes(int n,  boolean[] prime){
+        for(int i =2; i*i<=n; i++){
+            if(!prime[i]){
+                for(int j = i*2; j<=n; j+=i){
+                    prime[j] = true;
+                }
+            }
+        }
+
+        for(int i =2; i<=n; i++){
+            if(!prime[i]){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
