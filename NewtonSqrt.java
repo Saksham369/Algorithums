30b491e167b2ee15ef5d3d8c4c85770ac4e0751f
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-17 14:01:39 +0530

    Add files via upload

---
+++ NewtonSqrt.java
@@ -0,0 +1,24 @@
+package questions;
+
+public class NewtonSqrt {
+    //Newton Raphson Method
+
+    public static void main(String[] args){
+        System.out.println(sqrt(40));
+    }
+    static double sqrt(double n){
+        double x = n;
+        double root;
+        while(true){
+            root = 0.5 * ( x + (n/x));
+
+            if(Math.abs(root - x) < 0.5)//checking the error
+            {
+                break;
+            }
+         x =root;
+        }
+        return root;
+
+    }
+}
